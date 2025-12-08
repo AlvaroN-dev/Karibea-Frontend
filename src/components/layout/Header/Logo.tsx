c07e0753d5bb5d9b9c89dcdbfcc19f874b8e6179
Andres David Armenta Lopez <andresadalo25@gmail.com>
2025-12-08 00:42:40 -0500

    feat: se implemento el MobileMenu del header

--- src/components/layout/Header/Logo.tsx
+++ src/components/layout/Header/Logo.tsx
@@ -5,7 +5,6 @@ const Logo = ({className} : {className? :string}) => {
   return (
     <Link href={'/'}>
       <h2 className={cn('text-2xl text-darkColor font-black tracking-wider uppercase hover:text-lightColor hoverEffect group font-sans',className)}>
-
         KARIBEA
       </h2>
     </Link>
